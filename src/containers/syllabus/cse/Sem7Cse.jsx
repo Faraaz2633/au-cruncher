6fb33433aefae8f7813b3315d5fa1f9faf847597
Sakthi1112 <sakthikumar11122000@gmail.com>
2022-06-06 01:33:01 +0530

    syllabus-fix

--- src/containers/syllabus/cse/Sem7Cse.jsx
+++ src/containers/syllabus/cse/Sem7Cse.jsx
@@ -1,8 +1,163 @@
 import React from 'react'
+import Accordions from '../../../components/Accordions'
+
 
 const Sem7Cse = () => {
+  const data = [
+    {
+      subjectName: 'HS8151–Communicative English',
+        units:[
+          {
+            chapterName: 'Unit 1 -',
+            content: ''
+          },
+          {
+            chapterName: 'Unit 2 -',
+            content: ''
+          },
+          {
+            chapterName: 'Unit 3 -',
+            content: ''
+          },
+          {
+            chapterName: 'Unit 4 -',
+            content: ''
+          },
+          {
+            chapterName: 'Unit 5 -',
+            content: ''
+          }
+        ]
+      },
+      {
+        subjectName: 'HS8151–Communicative English',
+          units:[
+            {
+              chapterName: 'Unit 1 -',
+              content: ''
+            },
+            {
+              chapterName: 'Unit 2 -',
+              content: ''
+            },
+            {
+              chapterName: 'Unit 3 -',
+              content: ''
+            },
+            {
+              chapterName: 'Unit 4 -',
+              content: ''
+            },
+            {
+              chapterName: 'Unit 5 -',
+              content: ''
+            },
+          ]
+        }, {
+          subjectName: 'HS8151–Communicative English',
+            units:[
+              {
+                chapterName: 'Unit 1 -',
+                content: ''
+              },
+              {
+                chapterName: 'Unit 2 -',
+                content: ''
+              },
+              {
+                chapterName: 'Unit 3 -',
+                content: ''
+              },
+              {
+                chapterName: 'Unit 4 -',
+                content: ''
+              },
+              {
+                chapterName: 'Unit 5 -',
+                content: ''
+              },
+            ]
+          }, {
+            subjectName: 'HS8151–Communicative English',
+              units:[
+                {
+                  chapterName: 'Unit 1 -',
+                  content: ''
+                },
+                {
+                  chapterName: 'Unit 2 -',
+                  content: ''
+                },
+                {
+                  chapterName: 'Unit 3 -',
+                  content: ''
+                },
+                {
+                  chapterName: 'Unit 4 -',
+                  content: ''
+                },
+                {
+                  chapterName: 'Unit 5 -',
+                  content: ''
+                },
+              ]
+            },
+            {
+              subjectName: 'HS8151–Communicative English',
+                units:[
+                  {
+                    chapterName: 'Unit 1 -',
+                    content: ''
+                  },
+                  {
+                    chapterName: 'Unit 2 -',
+                    content: ''
+                  },
+                  {
+                    chapterName: 'Unit 3 -',
+                    content: ''
+                  },
+                  {
+                    chapterName: 'Unit 4 -',
+                    content: ''
+                  },
+                  {
+                    chapterName: 'Unit 5 -',
+                    content: ''
+                  },
+                ]
+              },
+              {
+                subjectName: 'HS8151–Communicative English',
+                  units:[
+                    {
+                      chapterName: 'Unit 1 -',
+                      content: ''
+                    },
+                    {
+                      chapterName: 'Unit 2 -',
+                      content: ''
+                    },
+                    {
+                      chapterName: 'Unit 3 -',
+                      content: ''
+                    },
+                    {
+                      chapterName: 'Unit 4 -',
+                      content: ''
+                    },
+                    {
+                      chapterName: 'Unit 5 -',
+                      content: ''
+                    },
+                  ]
+                }
+  ]
   return (
-    <div>Sem7Cse</div>
+    <div className="min-h-screen pt-12 m-auto w-[90%]">
+        <h1 className="text-xl font-semibold flex justify-center">CSE Sem - 7</h1>
+        <Accordions data={data} />
+    </div>
   )
 }
 
