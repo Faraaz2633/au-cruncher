c9a13d786ec3852d186ba9b9b71c37e81e54c1e0
Shriprasanna B <shriprasanna32@gmail.com>
2022-10-02 19:13:07 +0530

    added github and linkedin when hover

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import shri from "./shri.webp";
+import { FaGithub } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 
 const About = () => {
   return (
@@ -10,7 +12,7 @@ const About = () => {
         About us
       </h1>
       <main className="p-3 md:flex justify-center items-center flex-col">
-        {/* <section className="text-[18px] md:text-2xl">
+        <section className="text-[18px] md:text-2xl">
           An Multi purpose app created for Anna University Students, which
           includes
           <ul className="list-disc ml-9">
@@ -34,7 +36,7 @@ const About = () => {
               <span className="underline underline-offset-4 decoration-sky-500">
                 Attendance
               </span>{" "}
-              calculator and{" "}
+              calculator and
             </li>
 
             <li>
@@ -45,8 +47,8 @@ const About = () => {
             </li>
           </ul>{" "}
           <br />
-        </section> */}
-        {/* <h1 className="text-[18px] md:text-2xl">
+        </section>
+        {/* <h1 className="text-[18px] md:text-2xl text-center">
           An Multi purpose app created for Anna University Students which
           includes calculating GPA, CGPA and Attendance calculator. It also has
           all the syllabus for all the semester with notes.
@@ -55,19 +57,47 @@ const About = () => {
         {/* contributes  */}
         <section className="mt-7">
           <h1 className="text-3xl md:text-4xl text-center font font-semibold ">
-            Our Contributes
+            Our Contributers
           </h1>
           {/* main dev */}
-          <div className="flex justify-center mt-5 gap-6 flex-wrap group">
+          <div className="flex justify-center mt-5 gap-6 flex-wrap ">
             {/* cont div */}
-            {/* <div
+            <div
               className="w-[180px] border-2 border-sky-500 h-[190px]
-             rounded-md flex flex-col items-center bg-sky-500 
+             rounded-md flex flex-col items-center bg-sky-500 group
+              transition-all duration-500
             "
             >
-              <img src={shri} alt="sasa" className="rounded-md" />
-              <h1 className="p-1 text-[18px] font-semibold">Shriprasanna</h1>
-            </div> */}
+              <img
+                src={shri}
+                alt="sasa"
+                className="rounded-md group-hover:opacity-50"
+              />
+              <ul className="relative -top-[70px] flex gap-7 transition-all duration-200 opacity-0 group-hover:opacity-100">
+                <li>
+                  <a
+                    href="https://github.com/shrix1"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaGithub className="text-3xl hover:text-white text-black " />
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/shriprasanna-b-62aab1208/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <BsLinkedin className="text-3xl hover:text-white text-black" />
+                  </a>
+                </li>
+              </ul>
+              <h1 className="p-1 text-[18px] font-semibold -mt-[30px]">
+                Shriprasanna
+              </h1>
+            </div>
           </div>
         </section>
       </main>
