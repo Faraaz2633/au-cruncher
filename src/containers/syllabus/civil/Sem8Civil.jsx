ac626aa6f93ab61ee18d5cd825282e18ef8a47d8
Kiran Kumar K <kirankumarwaste1@gmail.com>
2022-10-03 16:32:51 +0530

    civil sem 8 added

--- src/containers/syllabus/civil/Sem8Civil.jsx
+++ src/containers/syllabus/civil/Sem8Civil.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Accordions from "../../../components/Accordions";
 
 const Sem8Civil = () => {
+
+  const data = [
+    {
+      subjectName: "Proffessional Elective IV",
+      units: [
+        {
+          chapterName:
+            "Professional Elective",
+          content:
+            " ",
+        },
+      ],
+    },
+    {
+      subjectName: "Open Elective V",
+      units: [
+        {
+          chapterName: "Open Elective",
+          content:
+            " ",
+        },
+      ],
+    },
+    {
+      subjectName: "CE8811 PROJECT WORK",
+      units: [
+        {
+          chapterName: "Main project",
+          content:
+            " ",
+        },
+      ],
+    },
+  ]
   return (
-    <div>Sem8Civil</div>
+    <div className="min-h-screen pt-12 m-auto w-[90%]">
+      <h1 className="text-xl font-semibold flex justify-center">CIVIL Sem - 8</h1>
+      <Accordions data={data} />
+    </div>
   )
 }
 
