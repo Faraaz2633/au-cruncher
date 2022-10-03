20ac0fd317af929ed7bedf011dcf56bc58a7070e
keerthivasan <keerthiva101@gmail.com>
2022-10-03 15:52:31 +0530

    updated 2

--- src/containers/syllabus/auto/Sem8Auto.jsx
+++ src/containers/syllabus/auto/Sem8Auto.jsx
@@ -9,27 +9,27 @@ const Sem8Auto = () => {
         {
           chapterName: "Unit 1 - CAR BODY DETAILS",
           content:
-          "",
+          "Types of Car body - Saloon, convertibles, Limousine, Estate Van, Racing and Sports car – Visibilityregulations, driver’s visibility, improvement in visibility and tests for visibility. Driver seat design -Carbody construction-Various panels in car bodies. Safety aspect of car body.",
         },
         {
           chapterName: "Unit 2 - BUS BODY DETAILS",
           content:
-          "",
+          "Types of bus body: based on capacity, distance traveled and based on construction.– Bus body layout for various types, Types of metal sections used – Regulations – Constructional details:Conventional and integral. driver seat design- Safety aspect of bus body.",
         },
         {
           chapterName: "Unit 3 - COMMERCIAL VEHICLE DETAILS",
           content:
-          "",
+          "Types of commercial vehicle bodies - Light commercial vehicle body. Construction details ofcommercial vehicle body - Flat platform body, Trailer, Tipper body and Tanker body – Dimensions ofdriver’s seat in relation to controls – Drivers cab design - Regulations.",
         },
         {
           chapterName: "Unit 4 - VEHICLE AERODYNAMICS",
           content:
-          "",
+          "Objectives, Vehicle drag and types. Various types of forces and moments. Effects of forces andmoments. Side wind effects on forces and moments. Various body optimization techniques forminimum drag. Wind tunnels – Principle of operation, Types. Wind tunnel testing such as: Flowvisualization techniques, Airflow management test – measurement of various forces and moments byusing wind tunnel.",
         },
         {
           chapterName: "Unit 5 - BODY MATERIALS, TRIM, MECHANISMS AND BODY REPAIR",
           content:
-          "",
+          "Types of materials used in body construction-Steel sheet, timber, plastics, GRP, properties ofmaterials. Body trim items-body mechanisms.Hand tools-power tools-panel repair-repairing sheetmetal-repairing plastics-body fillers-passenger compartment service- corrosion: Anticorrosionmethods, Modern painting process procedure-paint problems",
         },
       ],
     },
