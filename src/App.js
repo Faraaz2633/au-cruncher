8273c8d0628ac92f505d6496bfe222b65b7cdfe7
Mohammed Mothaseem Faraz <mdfaraz4343@gmail.com>
2022-10-04 16:22:37 +0530

    Revert "changed first and second sem in EEE"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import HomePage from './containers/HomePage/HomePage';
+import "./App.css";
+import HomePage from "./containers/HomePage/HomePage";
 
 function App() {
   return (
