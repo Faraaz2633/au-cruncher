8b303a819fe1619837b323dabae48f84c8ffb494
Kiran Kumar K <kirankumarwaste1@gmail.com>
2022-10-04 19:03:12 +0530

    civil 1,7,8 patch#1

--- src/containers/syllabus/civil/Sem8Civil.jsx
+++ src/containers/syllabus/civil/Sem8Civil.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
+import React from "react";
 import Accordions from "../../../components/Accordions";
 
 const Sem8Civil = () => {
-
   const data = [
     {
       subjectName: "Proffessional Elective IV",
       units: [
         {
-          chapterName:
-            "Professional Elective",
-          content:
-            " ",
+          chapterName: "Professional Elective",
+          content: " ",
         },
       ],
     },
@@ -20,8 +17,7 @@ const Sem8Civil = () => {
       units: [
         {
           chapterName: "Open Elective",
-          content:
-            " ",
+          content: " ",
         },
       ],
     },
@@ -30,18 +26,19 @@ const Sem8Civil = () => {
       units: [
         {
           chapterName: "Main project",
-          content:
-            " ",
+          content: " ",
         },
       ],
     },
-  ]
+  ];
   return (
     <div className="min-h-screen pt-12 m-auto w-[90%]">
-      <h1 className="text-xl font-semibold flex justify-center">CIVIL Sem - 8</h1>
+      <h1 className="text-xl font-semibold flex justify-center">
+        CIVIL Sem - 8
+      </h1>
       <Accordions data={data} />
     </div>
-  )
-}
+  );
+};
 
-export default Sem8Civil
+export default Sem8Civil;
